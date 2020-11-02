604b4387550c4aebc48d1d7ee754d73e2ade9f33
Raytwo <raytwost@gmail.com>
2020-11-02 23:36:23 +0100

    Fix comments

--- src/core/callbacks.rs
+++ src/core/callbacks.rs
@@ -4,17 +4,18 @@ use crate::core::{ ArcInfo, ArcFile };
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum CallbackType {
     File(u64),
-    /// Unused
+    /// Unused.
     Directory(u64),
     Extension(u64),
 }
 
-#[repr(C, u8)]
+#[repr(C)]
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
 pub enum ArcCallback {
     /// Does not work on Extension callbacks for the time being
     Listener(ListenerCallback),
-    /// Editors are meant to edit the content of a file, either the game's or the user's. They can provide their own filesize ahead of time if they plan on replacing with a larger file. Size goes unused for now
+    /// Editors are meant to edit the content of a file, either the game's or the user's. They can provide their own filesize ahead of time if they plan on replacing with a larger file.
+    // TODO: Make it so a size is not necessary, if possible?
     Editor(u32, EditorCallback),
     /// Replacers are meant to provide a file, no matter the size and have no intention of replacing one. You probably only want this for stream stuff, as other type don't play too well with that for the time being.
     Replacer(ReplacerCallback),
@@ -22,7 +23,7 @@ pub enum ArcCallback {
 
 // Hash
 pub type ListenerCallback = extern "C" fn(u64);
-/// ArcInfo, out_ArcFile
+/// ArcFile, out_ArcResult
 pub type EditorCallback = extern "C" fn(*const ArcInfo, *mut ArcFile) -> bool;
-/// Hash, out_ArcFile
+/// Hash, out_ArcResult
 pub type ReplacerCallback = extern "C" fn(u64, *mut ArcFile) -> bool;
